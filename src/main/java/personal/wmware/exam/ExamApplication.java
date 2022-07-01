8bbcfbad8408466414858530fef6c029580973b7
rosne <rosneromri@gmail.com>
2022-07-01 17:19:12 +0300

    embedded elasticsearch working

--- src/main/java/personal/wmware/exam/ExamApplication.java
+++ src/main/java/personal/wmware/exam/ExamApplication.java
@@ -1,13 +1,15 @@
 package personal.wmware.exam;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ExamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExamApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExamApplication.class, args);
+    }
 
 }
