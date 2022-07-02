3b2d094701ffd66393b776b5c96b609086957c74
rosne <rosneromri@gmail.com>
2022-07-02 10:57:43 +0300

    added more mappings for indices

--- src/main/java/personal/wmware/exam/elasticsearch/EmbeddedElasticConfig.java
+++ src/main/java/personal/wmware/exam/elasticsearch/EmbeddedElasticConfig.java
@@ -1,11 +1,9 @@
 package personal.wmware.exam.elasticsearch;
 
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 import java.util.Map;
 
 @Configuration
