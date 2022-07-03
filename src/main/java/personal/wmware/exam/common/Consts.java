cec393fb252e5ca27f49db4d1656ed4211162639
rosne <rosneromri@gmail.com>
2022-07-03 20:47:05 +0300

    added consts

---
+++ src/main/java/personal/wmware/exam/common/Consts.java
@@ -0,0 +1,7 @@
+package personal.wmware.exam.common;
+
+public class Consts {
+    public static final String OWNERS_SETTINGS = "owner";
+    public static final String STORE_SETTINGS = "store";
+
+}
